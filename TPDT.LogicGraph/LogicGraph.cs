41df1a7e3cf0ffa46adec41e07c9306e9244f618
tcz717 <tcz717@hotmail.com>
2013-12-21 17:33:45 +0800

    简单测试

--- TPDT.LogicGraph/LogicGraph.cs
+++ TPDT.LogicGraph/LogicGraph.cs
@@ -1,4 +1,6 @@
-﻿using SharpDX.Toolkit;
+﻿using SharpDX;
+using SharpDX.Toolkit;
+using SharpDX.Toolkit.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,17 +11,33 @@ namespace TPDT.LogicGraph
 {
     class LogicGraph:Game
     {
+        Texture2D tex;
         private GraphicsDeviceManager graphicsDeviceManager;
+        private SpriteBatch sb;
         public LogicGraph()
         {
             graphicsDeviceManager = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
         }
+        protected override void LoadContent()
+        {
+            tex = Content.Load<Texture2D>("Logo.jpg");
+            //tex = Texture2D.Load(GraphicsDevice, "Logo.jpg");
+            base.LoadContent();
+        }
         protected override void Initialize()
         {
             Window.Title = "1213";
+            sb = new SpriteBatch(GraphicsDevice);
 
             base.Initialize();
+        }
+        protected override void Draw(GameTime gameTime)
+        {
+            sb.Begin();
+            sb.Draw(tex, Vector2.Zero, Color.White);
+            sb.End();
+            base.Draw(gameTime);
         }
     }
 }
